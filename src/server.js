38b201508eadb9df76eeeeff5e26766c9c13d386
Kyle <kyle@kylemacquarrie.co.uk>
2020-03-27 22:50:50 +0000

    Refactor to use Preact components

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const express = require('express')
 const compression = require('compression')
 import render from 'preact-render-to-string'
 import { html } from 'htm/preact' // use the provided preact binding
+import PreactApp from './components/PreactApp'
 
 const app = express() // create the express app
 app.use(compression()) // use gzip for all requests
 
-const body = render(html`<h1>Hello from Preact</h1>`)
+const body = render(html`
+  <h1>Hello from Preact</h1>
+  <${PreactApp} />
+`)
 
 // some basic html to show
 const layout =`
